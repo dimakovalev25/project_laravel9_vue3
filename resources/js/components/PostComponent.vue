71fbde7d0b186a6ae8e0244b13715ada3f578a8d
dimakovalev <d.nozdryn@gmail.com>
2023-06-22 19:17:29 +0300

    v-if and v-for

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,30 +1,73 @@
 <template>
-    <div>
-        <single-post-component></single-post-component>
+    <div class="container">
+        <SinglePostComponent></SinglePostComponent>
         <h3>
             post component
+            {{ number }}
         </h3>
-
+        <h5>{{ getdata }}</h5>
         <button @click="sayHi">say hi</button>
-<!--        <div v-for="person in persons">-->
-<!--            <h3>{{person.name}}</h3>-->
-<!--            <h3>{{person.age}}</h3>-->
-<!--        </div>-->
+        <button @click="sayHello">say hello</button>
+        <button @click="plus1">plus1</button>
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+            </tr>
+            </thead>
+            <tbody>
+            <template v-for="user in users">
+                <tr v-if="user.age > 40">
+                    <th scope="row">{{ user.id }}</th>
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.age }}</td>
+                </tr>
+            </template>
+            </tbody>
+        </table>
+
     </div>
 </template>
 
 <script>
 import SinglePostComponent from "@/components/SinglePostComponent.vue";
+
 export default {
     components: {
-      SinglePostComponent
+        SinglePostComponent
     },
 
     name: "PostComponent",
 
+    computed: {
+        getdata() {
+            return 'test data'
+        }
+    },
+
     data() {
         return {
-            persons: null
+            number: 44,
+            users: [
+                {
+                    name: 'jo',
+                    age: 33,
+                    id: 1
+                },
+                {
+                    name: 'annett',
+                    age: 31,
+                    id: 2
+                },
+                {
+                    name: 'alex',
+                    age: 43,
+                    id: 3
+                }
+            ]
         }
     },
 
@@ -41,7 +84,7 @@ export default {
                     console.log(data)
                     console.log(data.data)
                 })
-                .then(data=>{
+                .then(data => {
                     console.log(data);
                 })
 
@@ -56,8 +99,16 @@ export default {
                 })
         },
 
-        sayHi(){
+        sayHi() {
             console.log('hi!!!!!!!!');
+        },
+
+        sayHello() {
+            console.log('hello!');
+        },
+
+        plus1() {
+            this.number = this.number + 1;
         }
     }
 }
